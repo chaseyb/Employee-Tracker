dbbbeb6e7978e7b14dc482bf4703ce7ad0612286
Chase Johnson <Chase.b.johnson@gmail.com>
2020-07-04 14:02:04 -0700

    Fixed issue with sql db not loading tables.

--- db/schema.sql
+++ db/schema.sql
@@ -1,31 +1,26 @@
 DROP DATABASE IF EXISTS employeesdb;
 CREATE DATABASE employeesdb;
-
 USE employeesdb;
 
 CREATE TABLE department (
     id INT NOT NULL AUTO_INCREMENT,
-    department_name VARCHAR(30) NOT NULL,
+    department_name VARCHAR(30),
     PRIMARY KEY(id)
 );
 
 CREATE TABLE role (
     id INT NOT NULL AUTO_INCREMENT,
-    title VARCHAR(30) NOT NULL,
-    salary DECIMAL(10, 2) NOT NULL,
-    department_id INT NOT NULL,
+    title VARCHAR(30),
+    salary DECIMAL(10, 2),
+    department_id INT,
     PRIMARY KEY(id)
 );
 
 CREATE TABLE employee (
     id INT NOT NULL AUTO_INCREMENT,
-    first_name VARCHAR(30) NOT NULL,
-    last_name VARCHAR(30) NOT NULL,
-    role_id INT NOT NULL,
-    manager_id INT NOT NULL,
+    first_name VARCHAR(30),
+    last_name VARCHAR(30),
+    role_id INT,
+    manager_id INT,
     PRIMARY KEY(id)
 );
-
-SELECT * FROM department;
-SELECT * FROM role;
-SELECT * FROM employee;
